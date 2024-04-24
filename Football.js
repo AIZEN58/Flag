87c13a5dd14517ee1f087b05c1253e47642b31db
AIZEN58 <162022243+AIZEN58@users.noreply.github.com>
2024-04-25 01:06:51 +0300

    Create Football.js

---
+++ Football.js
@@ -0,0 +1,163 @@
+
+[{
+question": "https://telegra.ph/file/59ba527ddacb117027167.jpg",
+ "response": "هالاند" } ,{
+    "question": "https://telegra.ph/file/325b1cbeb3b89f100f3ec.jpg",
+ "response": "ابراهيموفيتش" } ,{
+    "question":"https://telegra.ph/file/1b581058e081c7a11f7b2.jpg",
+ "response": "يوهان كرويف" } ,{
+    "question": "https://telegra.ph/file/cfe3405bed769bbdc20b9.jpg",
+ "response": "مالديني" } ,{
+    "question": "https://telegra.ph/file/dd0936ceda70dbc9e0b3d.jpg",
+ "response": "فودين" } ,{
+    "question": "https://telegra.ph/file/9d23b383f3482c49ff0c4.jpg",
+ "response": "بنزيما" } ,{
+    "question": "https://telegra.ph/file/c4b3ac371bf48abbec64d.jpg",
+ "response": "جيرو" } ,{
+    "question": "https://telegra.ph/file/a8dbf9497e8b0ace0a093.jpg",
+ "response": "لوكاكو" } ,{
+    "question": "https://telegra.ph/file/a8dbf9497e8b0ace0a093.jpg",
+ "response": "ديبروين" } ,{
+    "question": "https://telegra.ph/file/3dcd2be1e4a06389952bb.jpg",
+ "response": "كورتوا" } ,{
+    "question": "https://telegra.ph/file/5a2b340e87b52024627a3.jpg",
+ "response": "مارتينيز" } ,{
+    "question": "https://telegra.ph/file/34444279b5717b672e6b8.jpg",
+ "response": "انطوني" } ,{
+    "question": "https://telegra.ph/file/0174744527c961c3aa7cd.jpg",
+ "response": "فينيسيوس" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Bahrain.svg/800px-Flag_of_Bahrain.svg.png",
+ "response": "البحرين" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Flag_of_Bangladesh.svg/800px-Flag_of_Bangladesh.svg.png",
+ "response": "بنغلاديش" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Flag_of_Barbados.svg/800px-Flag_of_Barbados.svg.png",
+ "response": "بربادوس" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Flag_of_Belarus.svg/800px-Flag_of_Belarus.svg.png",
+ "response": "بيلاروسيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Belgium.svg/800px-Flag_of_Belgium.svg.png",
+ "response": "بلجيكا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Flag_of_Belize.svg/800px-Flag_of_Belize.svg.png",
+ "response": "بليز" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Flag_of_Benin.svg/800px-Flag_of_Benin.svg.png",
+ "response": "بنين" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Flag_of_Bhutan.svg/800px-Flag_of_Bhutan.svg.png",
+ "response": "بوتان" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Flag_of_Bolivia.svg/800px-Flag_of_Bolivia.svg.png",
+ "response": "بوليفيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Flag_of_Bosnia_and_Herzegovina.svg/800px-Flag_of_Bosnia_and_Herzegovina.svg.png",
+ "response": "البوسنة و الهرسك" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_Botswana.svg/800px-Flag_of_Botswana.svg.pnghttps://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_Botswana.svg/800px-Flag_of_Botswana.svg.png",
+ "response": "بوتسوانا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/800px-Flag_of_Brazil.svg.png",
+ "response": "البرازيل" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Brunei.svg/800px-Flag_of_Brunei.svg.png",
+ "response": "بروناي" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Bulgaria.svg/800px-Flag_of_Bulgaria.svg.png",
+ "response": "بلغاريا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Flag_of_Burkina_Faso.svg/800px-Flag_of_Burkina_Faso.svg.png",
+ "response": "بوركينا فاسو" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Flag_of_Burundi.svg/800px-Flag_of_Burundi.svg.png",
+ "response": "بوروندي" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Flag_of_Cambodia.svg/800px-Flag_of_Cambodia.svg.png",
+ "response": "كمبوديا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Flag_of_Cameroon.svg/800px-Flag_of_Cameroon.svg.png",
+ "response": "الكاميرون" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/800px-Flag_of_Canada_%28Pantone%29.svg.png",
+ "response": "كندا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Flag_of_Cape_Verde.svg/1280px-Flag_of_Cape_Verde.svg.png",
+ "response": "الرأس الأخضر" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Flag_of_the_Central_African_Republic.svg/1280px-Flag_of_the_Central_African_Republic.svg.png",
+ "response": "جمهورية افريقيا الوسطى" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Flag_of_Chad.svg/1280px-Flag_of_Chad.svg.png",
+ "response": "تشاد" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Flag_of_Chile.svg/1024px-Flag_of_Chile.svg.png",
+ "response": "شيلي" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/1024px-Flag_of_the_People%27s_Republic_of_China.svg.png",
+ "response": "الصين" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/1024px-Flag_of_Colombia.svg.png",
+ "response": "كولومبيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Flag_of_the_Comoros.svg/1024px-Flag_of_the_Comoros.svg.png",
+ "response": "جزر القمر" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Flag_of_the_Democratic_Republic_of_the_Congo.svg/1024px-Flag_of_the_Democratic_Republic_of_the_Congo.svg.png",
+ "response": "الكونغو" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Flag_of_the_Republic_of_the_Congo.svg/1024px-Flag_of_the_Republic_of_the_Congo.svg.png",
+ "response": "الكونغو" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Costa_Rica.svg/1024px-Flag_of_Costa_Rica.svg.png",
+ "response": "كوستا ريكا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/1024px-Flag_of_Croatia.svg.png",
+ "response": "كرواتيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Flag_of_Cuba.svg/1024px-Flag_of_Cuba.svg.png",
+ "response": "كوبا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Flag_of_Cyprus.svg/1024px-Flag_of_Cyprus.svg.png",
+ "response": "قبرص" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_Czech_Republic.svg/1024px-Flag_of_the_Czech_Republic.svg.png",
+ "response": "التشيكية" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/1024px-Flag_of_Denmark.svg.png",
+ "response": "الدنمارك" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Flag_of_Djibouti.svg/1024px-Flag_of_Djibouti.svg.png",
+ "response": "جيبوتي" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Flag_of_Dominica.svg/1024px-Flag_of_Dominica.svg.png",
+ "response": "دومينيكا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_the_Dominican_Republic.svg/1024px-Flag_of_the_Dominican_Republic.svg.png",
+ "response": "الدومينيكان" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Flag_of_East_Timor.svg/1024px-Flag_of_East_Timor.svg.png",
+ "response": "تيمور الشرقية" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/1024px-Flag_of_Ecuador.svg.png",
+ "response": "الاكوادور" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/1024px-Flag_of_Egypt.svg.png",
+ "response": "مصر" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Flag_of_El_Salvador.svg/1024px-Flag_of_El_Salvador.svg.png",
+ "response": "السلفادور" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Flag_of_Equatorial_Guinea.svg/1024px-Flag_of_Equatorial_Guinea.svg.png",
+ "response": "غينيا الإستوائية" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Flag_of_Eritrea.svg/1024px-Flag_of_Eritrea.svg.png",
+ "response": "إريتريا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Flag_of_Estonia.svg/1024px-Flag_of_Estonia.svg.png",
+ "response": "إستونيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Flag_of_Eswatini.svg/1024px-Flag_of_Eswatini.svg.png",
+ "response": "إيسواتيني" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Flag_of_Ethiopia.svg/1024px-Flag_of_Ethiopia.svg.png",
+ "response": "أثيوبيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Fiji.svg/1024px-Flag_of_Fiji.svg.png",
+ "response": "فيجي" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/1024px-Flag_of_Finland.svg.png",
+ "response": "فنلندا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/1024px-Flag_of_France.svg.png",
+ "response": "فرنسا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Flag_of_Gabon.svg/1024px-Flag_of_Gabon.svg.png",
+ "response": "الجابون" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_The_Gambia.svg/1024px-Flag_of_The_Gambia.svg.png",
+ "response": "غامبيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Flag_of_Georgia.svg/1024px-Flag_of_Georgia.svg.png",
+ "response": "جورجيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1024px-Flag_of_Germany.svg.png",
+ "response": "ألمانيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Ghana.svg/1024px-Flag_of_Ghana.svg.png",
+ "response": "غانا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/1024px-Flag_of_Greece.svg.png",
+ "response": "اليونان" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Grenada.svg/1024px-Flag_of_Grenada.svg.png",
+ "response": "غرينادا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Flag_of_Guatemala.svg/1024px-Flag_of_Guatemala.svg.png",
+ "response": "غواتيمالا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Flag_of_Guinea.svg/1024px-Flag_of_Guinea.svg.png",
+ "response": "غينيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Flag_of_Guinea-Bissau.svg/1024px-Flag_of_Guinea-Bissau.svg.png",
+ "response": "غينيا بيساو" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Flag_of_Guyana.svg/1024px-Flag_of_Guyana.svg.png",
+ "response": "غيانا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Flag_of_Haiti.svg/1024px-Flag_of_Haiti.svg.png",
+ "response": "هايتي" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Flag_of_Honduras.svg/1024px-Flag_of_Honduras.svg.png",
+ "response": "هندوراس" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Flag_of_Hungary.svg/1024px-Flag_of_Hungary.svg.png",
+ "response": "هنغاريا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/1024px-Flag_of_Iceland.svg.png",
+ "response": "أيسلندا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1024px-Flag_of_India.svg.png",
+ "response": "الهند" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_Indonesia.svg/1024px-Flag_of_Indonesia.svg.png",
+ "response": "إندونيسيا" } ,{
+    "question": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/1024px-Flag_of_Iran.svg.png",
+ "response": "إيران" } 
+]
